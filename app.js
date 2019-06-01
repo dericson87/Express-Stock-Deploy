e944e4942b93c10120c8e2bea94b14b43081ec77
Derek Ericson <derek.ericson@gmail.com>
2019-05-31 23:10:16 -0700

    Enable CORS

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // Loggin code
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
